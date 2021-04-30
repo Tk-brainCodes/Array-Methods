da778ee463fc8e7cc2c4e217ecd6ad5fb6bc3f3f
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-04-30 09:13:48 +0100

    fetch data  from api

---
+++ Fetch.js
@@ -0,0 +1,11 @@
+let records = [
+    {
+        profiles: {
+            0: {FirstName: "ThankGod", LastName: "Chuks", Gender: "Male" },
+            1: {FirstName: "Rhoda", LastName: "rose", Gender: "Female" },
+            2: {FirstName: "Favour", LastName: "flower", Gender: "Female" },
+        }
+    }
+]
+
+console.log(records);
